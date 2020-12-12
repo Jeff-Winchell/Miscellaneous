7422a9d36d2ebcf107fa9f7706d41ab972b4f619
Jeff Winchell <Jeff.Winchell@alumni.Northwestern.edu>
2020-12-12 04:52:07 -0500

    Create SCOTUS_v_GOPIdiots.py

---
+++ SCOTUS_v_GOPIdiots.py
@@ -0,0 +1,6 @@
+SCOTUS_v_GOPIdiots = "    The State of Texas's motion for leave to file a bill of complaint is denied for lack of standing under Article III of the Constitution.  Texas has not demonstrated "\
++"a judicially cognizable interest in the manner in which another State conducts its election.  All other pending motions are dismissed as moot."\
++"    Statement of Justice Alito, with whom Justice Thomas joins: "\
++"In my view, we do not have discretion to deny the filing of a bill of complaint in a case that falls within our original juridiction.   See Arizona v. California, 589 U. S. ____ (Feb. 24, 2020) (Thomas, J., dissenting).  I would therefore grant the motion to file the bill of complaint but would not grant other relief, and I express no view on any other issue."
+
+print(len(SCOTUS_v_Idiots.split()))
